a72db76537d05566c18666a7dfe86c64998b3176
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-13 13:43:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,7 @@ var playlist = new object(){
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName){
