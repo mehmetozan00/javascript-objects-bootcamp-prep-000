a39d8ee5d0d200f7fec989df081e5586639175e6
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-13 13:36:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,9 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = "songTitle";
+  playlist.artistName = 'songTitle';
+}
+
+function removeFromPlaylist(playlist, artistName){
+
 }
